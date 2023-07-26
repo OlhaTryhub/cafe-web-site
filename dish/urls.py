4c7705b51154127466807729d973c02a721ed1f2
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-26 03:51:27 +0300

    Add functionality for creating, updating and deleting dishes if user is super

--- dish/urls.py
+++ dish/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from dish.views import dish_type_view, DishDetailView, add_dish_to_users_order_view
+from dish.views import (dish_type_view,
+                        add_dish_to_users_order_view,
+                        DishDetailView,
+                        DishCreateView,
+                        DishDeleteView,
+                        DishUpdateView)
 
 urlpatterns = [
-    path("<str:dish_type>/", dish_type_view, name="dish-list"),
     path("details/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("details/<int:pk>/add_to_order/", add_dish_to_users_order_view, name="add-to-order"),
+    path("create/", DishCreateView.as_view(), name="dish-create"),
+    path("<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
+    path("<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("<str:dish_type>/", dish_type_view, name="dish-list"),
 ]
 
 app_name = "dish"
--- dish/views.py
+++ dish/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -32,3 +33,41 @@ def add_dish_to_users_order_view(request: HttpRequest,
                             pk: int) -> HttpResponseRedirect:
     _, dish_id = add_dish_to_users_order(request, pk)
     return HttpResponseRedirect(reverse_lazy("dish:dish-detail", args=[dish_id]))
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    fields = "__all__"
+    template_name = "dish/dish_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dish_types"] = DishType.objects.all()
+        return context
+
+    def get_success_url(self):
+        dish_type_name = self.object.type.name
+        return reverse_lazy("dish:dish-list", args=[dish_type_name])
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+
+    def get_success_url(self):
+        dish_type_name = self.object.type.name
+        return reverse_lazy("dish:dish-list", args=[dish_type_name])
+
+
+class DishUpdateView(generic.UpdateView):
+    model = Dish
+    fields = "__all__"
+    template_name = "dish/dish_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dish_types"] = DishType.objects.all()
+        return context
+
+    def get_success_url(self):
+        dish_type_name = self.object.id
+        return reverse_lazy("dish:dish-detail", args=[dish_type_name])
