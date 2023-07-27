9a18fa2cb1b80db3e198969f711ce3e25cf38cba
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-27 19:01:08 +0300

    Update settings.py for deploying

--- Cafe_feeling_happy/settings.py
+++ Cafe_feeling_happy/settings.py
@@ -3,11 +3,11 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-=-79j*as@=(fj=f#cvc&4$lnnu)d1p%^n)*n&b$_cvj9i5r$a#"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-=-79j*as@=(fj=f#cvc&4$lnnu)d1p%^n)*n&b$_cvj9i5r$a#")
 
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
