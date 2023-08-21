c6bd37d44b51e611cd14bd06df913dde0e28c795
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-21 20:46:40 +0300

    Remove unnessesary file forms.py

--- dish/forms.py
+++
@@ -1,5 +0,0 @@
-import django_filters
-from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
